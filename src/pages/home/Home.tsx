fde48abc97cbc4b8d2815b6279d7a521e6d6f34e
antiderson <andersondaniel.adfa@gmail.com>
2025-07-17 00:34:55 -0300

    First Commit

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Logo from '../../assets/LogoSimra.png'
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Simra App</Text>
+        <Image source={Logo} style={{ width: 200, height: 200 }} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
