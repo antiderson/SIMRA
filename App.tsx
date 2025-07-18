ae709567133d185f0a952420947a73c0d9c3f8cf
Anderson Daniel <andersondaniel.adfa@gmail.com>
2025-07-18 17:45:12 -0300

    Screens and functions and firebase config

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/pages/home/Home';
 import SplashScreen from './src/pages/splashScreen/SplashScreen';
 
+
 export default function App() {
   return (
     <>
-      <SplashScreen />
-      {/* <Home /> */}
+      {/* <SplashScreen /> */}
+        <Home />
     </>
   );
 }
