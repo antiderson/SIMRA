336a2459039bab974908d9d00f0332fa85148518
Anderson Daniel <andersondaniel.adfa@gmail.com>
2025-08-13 11:27:24 -0300

    Integrate Tamagui and add home/login pages
    Added Tamagui configuration files and assets to enable Tamagui UI integration. Created new Home and Login pages with associated assets and styles. Updated App.tsx, index.html, package.json, and vite.config.ts to support Tamagui and new pages.

--- simraWeb/src/App.tsx
+++ simraWeb/src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { TamaguiProvider } from 'tamagui'
 import './App.css'
+import Login from './pages/login/Login'
+import tamaguiConfig from './tamagui.config'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +26,11 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      {/* <Home /> */}
+      <TamaguiProvider config={tamaguiConfig}>
+        <Login />
+      </TamaguiProvider>
     </>
   )
 }
